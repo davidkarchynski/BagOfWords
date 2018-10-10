98e2157a49126de4be1e910e439adfd3b41f628a
TruemanL <truemanlam@gmail.com>
2018-10-10 15:29:10 -0700

    implemented vectorizeSentence (with test cases)

--- Vectorizer.hs
+++ Vectorizer.hs
@@ -6,5 +6,15 @@ import Parser
 -- corresponds to a Gram at position n in Corpus: 1 when Gram is in the sentence
 -- 0 when Gram is missing. length of Corpus = length of output list
 vectorizeSentence :: Corpus -> Sentence -> [Int]
-vectorizeSentence [] s = []
--- vectorizeSentence c s = ...
+vectorizeSentence c s = foldr (\ corpusGram acc -> if (elem corpusGram s) then 1:acc else 0:acc) [] c
+
+-- vectorizeSentence ["alert", "password", "security", "username"] ["please", "give", "us", "your", "username", "and", "password"]
+--      should return [0, 1, 0, 1]
+-- vectorizeSentence ["alert", "password", "security", "username"] ["please", "give", "us", "your", "first", "and", "last", "name"]
+--      should return [0, 0, 0, 0]
+-- vectorizeSentence ["alert", "password", "security", "username"] ["security", "alert", "we", "need", "your", "username", "and", "password"]
+--      should return [1, 1, 1, 1]
+-- vectorizeSentence [] ["security", "alert", "we", "need", "your", "username", "and", "password"]
+--      should return []
+-- vectorizeSentence ["alert", "password", "security", "username"] []
+--      should return [0, 0, 0, 0]
