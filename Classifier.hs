086a0cf7f45b4a9b1fb43ede718b9eda7041df7e
TruemanL <truemanlam@gmail.com>
2018-10-11 14:52:44 -0700

    refactored classifySentence

--- Classifier.hs
+++ Classifier.hs
@@ -11,29 +11,36 @@ type Matrix = [Vector]  -- note that each vector is a row in the matrix
 -- input vector is vectorized sentence we want to classify
 -- returns true if sentence classified as spam and false otherwise
 classifySentence :: Matrix -> Matrix -> Vector -> Bool
-classifySentence spamM hamM v = if (pSpam > pHam) then True else False
+classifySentence spamM hamM v = (pSpam > pHam)
                                 where 
                                      condPSpam = map (fst) condPs           
                                      margPSpam = (fromIntegral $ length spamM)/(fromIntegral $ (length spamM) + (length hamM))
-                                     pSpam = (foldr(*) 1 condPSpam)*margPSpam
+                                     pSpam = (product condPSpam)*margPSpam
                                      
                                      condPHam = map (snd) condPs
                                      margPHam = 1 - margPSpam
-                                     pHam = (foldr(*) 1 condPHam)*margPHam
+                                     pHam = (product condPHam)*margPHam
  
-                                     condPs = map (\(x,y) -> let intX = fromIntegral x
-                                                                 intY = fromIntegral y
-                                                                 intSum = fromIntegral (x + y) in
-                                                             (intX/intSum, intY/intSum)) 
-                                                 zippedCounts
+                                     condPs = getCondProb (zip spamCount hamCount)
                                      spamCount = getAllCounts spamM v
                                      hamCount = getAllCounts hamM v
-                                     zippedCounts = (zip spamCount hamCount)
 -- some basic tests
 -- classifySentence [[1, 0]] [[0, 1]] [1, 0] should be True
 -- classifySentence [[1, 0]] [[0, 1]] [0, 1] should be False
 -- classifySentence [[1, 0, 0, 0], [1, 1, 0, 0], [1, 0, 0, 1]] [[0, 1, 0, 0], [0, 0, 1, 1], [0, 0, 0, 1]] [1, 0, 0, 0] should be True
 
+-- given a list of (spamCount, hamCount) zipped tuples
+-- returns the conditional probability of each as (spamCount/totalCount, hamCount/totalCount)
+getCondProb :: (Integral a, Fractional b) => [(a, a)] -> [(b, b)]
+getCondProb zippedCounts = map (\(x,y) -> let intX = fromIntegral x
+                                              intY = fromIntegral y
+                                              intSum = intX + intY in
+                                              (intX/intSum, intY/intSum)) 
+                                          zippedCounts
+-- getCondProb [(0, 1), (1,0)] should return [(0.0, 1.0), (1.0, 0.0)]
+-- getCondProb [(1, 1), (2,2)] should return [(0.5, 0.5), (0.5, 0.5)]
+-- getCondProb [(3, 1), (1,4)] should return [(0.75, 0.25), (0.2, 0.8)]
+ 
 -- given a reference matrix mtrx and a target vector vctr
 -- returns a vector of with each entry i = the number of matches between vctr's i'th entry and mtrx's i'th entry in each of its row
 getAllCounts :: Matrix -> Vector -> [Int]
