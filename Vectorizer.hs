9ee075d3d87be87a1548be55302ff81af7b82c31
TruemanL <truemanlam@gmail.com>
2018-10-16 15:47:34 -0700

    additional improvement to Vectorizer

--- Vectorizer.hs
+++ Vectorizer.hs
@@ -2,16 +2,24 @@ module Vectorizer where
 
 -- cabal install sparse-linear-algebra-0.3.1
 import Data.Sparse.SpVector (fromListSV, SpVector, dotS)
+import Data.List
+import Data.Maybe
 import Parser
-import Data.Foldable (foldl')
 import CustomTypes
+import Corpufier (removeDups)
 
 -- Produce a sparse vector of 0s and 1s, where a number at position n in output list
 -- corresponds to a Gram at position n in Corpus: 1 when Gram is in the sentence
 -- 0 when Gram is missing. length of Corpus = length of output list
 vectorizeSentence :: Corpus -> Sentence -> (Int, [(Int, Int)])
-vectorizeSentence c s  = (length c, foldl' (\acc (gr, indx) -> if (elem gr s) then acc++[(indx, 1)] else acc) [] indexedCorpus)
-                            where indexedCorpus = zip c [0..]
+vectorizeSentence c s  = (corpusLength, vectorizedSentence)
+    where 
+          vectorizedSentence = foldr (\ gram acc -> let maybeIndex = (elemIndex gram c) in
+                                                        if (isNothing maybeIndex)
+                                                        then acc
+                                                        else (fromJust maybeIndex, 1):acc) [] processedSentence
+          processedSentence = removeDups s
+          corpusLength = length c
 
 -- vectorizeSentence ["alert", "password", "security", "username"] ["please", "give", "us", "your", "username", "and", "password"]
 --      should return (4,[(1,1),(3,1)])
@@ -24,7 +32,7 @@ vectorizeSentence c s  = (length c, foldl' (\acc (gr, indx) -> if (elem gr s) th
 -- vectorizeSentence ["alert", "password", "security", "username"] []
 --      should return (4, [])
 
--- sparsifyVectSentence :: Foldable t => (Int, t (Int, a)) -> SpVector a
+sparsifyVectSentence :: Foldable t => (Int, t (Int, a)) -> SpVector a
 sparsifyVectSentence (size, v) = fromListSV size v
 
 -- sparsifyVectSentence (4,[(1,1),(3,1)]) should be SV (4) [(1,1),(3,1)]
